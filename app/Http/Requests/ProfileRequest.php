de82af6b2672edc654a8e28d2ebc2c35b6ded30c
Diego Pereira Betti <diegopbetti.gmail.com>
2025-02-02 21:54:11 -0300

    Adiciona campo de upload de arquivos

--- app/Http/Requests/ProfileRequest.php
+++ app/Http/Requests/ProfileRequest.php
@@ -5,7 +5,11 @@ namespace App\Http\Requests;
 use App\Rules\CheckHandler;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
+use Illuminate\Http\UploadedFile;
 
+/**
+ * @property-read UploadedFile $photo
+ */
 class ProfileRequest extends FormRequest
 {
     /**
@@ -26,6 +30,7 @@ class ProfileRequest extends FormRequest
         return [
             'name' => ['required', 'min:3, max:30'],
             'description' => ['nullable'],
+            'photo' => ['nullable', 'image'],
             'handler' => ['required',
                 Rule::unique('users')-> ignoreModel($this->user()),
                 new  CheckHandler
